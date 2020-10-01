e9b4b8ad139c05aca62d0394506da8f5b5aefaab
Shahid <shahid@Shahanas-MacBook-Pro.local>
2020-10-01 03:41:36 -0400

    auto layout added; Missing EC

--- flixter/flixter/MovieDetailsViewController.swift
+++ flixter/flixter/MovieDetailsViewController.swift
@@ -6,13 +6,43 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropImageView: UIImageView!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String:Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterImageView.af_setImage(withURL: posterUrl!)
+        
+        let backdropImagePath = movie["backdrop_path"] as! String
+        let backdropImageUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropImagePath)
+        
+        backdropImageView.af_setImage(withURL: backdropImageUrl!)
+        
     }
     
 
--- flixter/flixter/MoviesViewController.swift
+++ flixter/flixter/MoviesViewController.swift
@@ -23,7 +23,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
 
         // Do any additional setup after loading the view.
-        print("Hello World")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -39,7 +38,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             
               self.tableView.reloadData()
             
-              print(dataDictionary)
             
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
@@ -74,14 +72,25 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
