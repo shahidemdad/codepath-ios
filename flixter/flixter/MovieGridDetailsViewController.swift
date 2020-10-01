a95f696124b5a8c723b578c03e675823521ab2bc
Shahid <shahid@Shahanas-MacBook-Pro.local>
2020-10-01 06:05:46 -0400

    completed EC 1/2

---
+++ flixter/flixter/MovieGridDetailsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MovieGridDetailsViewController.swift
+//  flixter
+//
+//  Created by Shahid on 10/1/20.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridDetailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backdropImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String:Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+         
+        posterImageView.af_setImage(withURL: posterUrl!)
+        
+        let backdropImagePath = movie["backdrop_path"] as! String
+        let backdropImageUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropImagePath)
+
+        backdropImageView.af_setImage(withURL: backdropImageUrl!)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- flixter/flixter/MovieGridViewController.swift
+++ flixter/flixter/MovieGridViewController.swift
@@ -12,7 +12,6 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
     var movies = [[String:Any]]()
 
     override func viewDidLoad() {
@@ -71,14 +70,27 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+//        print("loding up")
+        
+        // Find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        
+        
     }
-    */
+    
 
 }
